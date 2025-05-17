2aa3c88692df14f3fa7c88eef9a5eec0ceb1c04b
Aseel <aseelarr@gmail.com>
2025-05-17 21:33:55 +0000

    test automation

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,5 +1,6 @@
 import os
 from fastapi.testclient import TestClient
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
 client = TestClient(app)
