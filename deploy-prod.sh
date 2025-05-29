9c301c0cf1870d6d4a994d1e83104af6c77d40ca
Aseel <aseelarr@gmail.com>
2025-05-29 08:29:40 +0000

    fix deploy file

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,7 +4,17 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 PROJECT_DIR="/home/ubuntu/YoloService"
 VENV_DIR="$PROJECT_DIR/.venv"
-SERVICE_NAME="yolo-prod-service"
+SERVICE_NAME="yolo-prod.service"
+
+
+echo "🐍 Detecting Python version..."
+PYTHON_VERSION=$(python3 -c "import sys; print(f'python{sys.version_info.major}.{sys.version_info.minor}')")
+VENV_PACKAGE="${PYTHON_VERSION}-venv"
+
+echo "🧪 Ensuring $VENV_PACKAGE is installed..."
+sudo apt-get update
+sudo apt-get install -y "$VENV_PACKAGE"
+
 
 echo "📁 Navigating to project directory: $PROJECT_DIR"
 cd "$PROJECT_DIR"
@@ -13,7 +23,15 @@ cd "$PROJECT_DIR"
 if [ ! -d "$VENV_DIR" ]; then
   echo "🐍 Creating virtual environment..."
   python3 -m venv "$VENV_DIR"
+
+  if [ ! -f "$VENV_DIR/bin/activate" ]; then
+    echo "❌ Failed to create virtual environment. Exiting."
+    exit 1
+  fi
+else
+  echo "📦 Existing virtual environment found, reusing it."
 fi
+
 
 # Step 2: Activate virtual environment
 echo "🔌 Activating virtual environment..."
@@ -26,29 +44,33 @@ PACKAGE_TO_CHECK="fastapi"
 
 if ! pip show "$PACKAGE_TO_CHECK" > /dev/null 2>&1; then
   echo "📦 Installing Python dependencies..."
+
+  echo "🧮 Disk space before install:"
+  df -h
+
   pip install --upgrade pip
-  pip install -r requirements.txt
   pip install -r torch-requirements.txt
+  pip install -r requirements.txt
 else
   echo "✅ Dependencies already installed, skipping pip install."
 fi
 
 # Step 4: Copy the systemd service file
 echo "🛠️  Setting up systemd service..."
-sudo cp "$PROJECT_DIR/yolo.service" /etc/systemd/system/yolo.service
+sudo cp "$PROJECT_DIR/$SERVICE_NAME" /etc/systemd/system/$SERVICE_NAME
 
 # Step 4: Reload systemd and restart the service
 echo "🔄 Reloading systemd and restarting YOLO service..."
 sudo systemctl daemon-reload
-sudo systemctl restart yolo.service
-sudo systemctl enable yolo.service
+sudo systemctl restart $SERVICE_NAME
+sudo systemctl enable $SERVICE_NAME
 
 # Step 5: Check service status
 echo "🔍 Checking yolo.service status..."
-if systemctl is-active --quiet yolo.service; then
+if systemctl is-active --quiet $SERVICE_NAME  ; then
   echo "✅ yolo.service is running."
 else
   echo "❌ yolo.service failed to start."
-  sudo systemctl status yolo.service --no-pager
+  sudo systemctl status $SERVICE_NAME --no-pager
   exit 1
 fi
